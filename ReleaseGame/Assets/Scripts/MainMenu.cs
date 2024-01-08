5d813b45d1f3c3ee8e8b6000a0f27b693d8d3aff
simodewit <117769294+simodewit@users.noreply.github.com>
2024-01-08 16:47:31 +0100

    Start of the game
    The first push with all the materials, scenes, prefabs, folders etc. to start off in a nice fresh project. I even added the first ui to go through

---
+++ ReleaseGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,81 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    #region variables
+
+    [Header("All the refrences to the panels")]
+    public GameObject mainPanel;
+    public GameObject settingsPanel;
+    public GameObject playPanel;
+    public GameObject quitGamePanel;
+
+    public AudioSource buttonClickSound;
+    public int sceneLoadIndex;
+
+    #endregion
+
+    #region button functions
+
+    public void OnClickPlay()
+    {
+        //buttonClickSound.Play();
+        mainPanel.SetActive(false);
+        playPanel.SetActive(true);
+    }
+
+    public void OnClickJoinGame()
+    {
+        //buttonClickSound.Play();
+        SceneManager.LoadScene(sceneLoadIndex);
+    }
+
+    public void OnClickSettings()
+    {
+        //buttonClickSound.Play();
+        mainPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void OnClickQuitGame()
+    {
+        //buttonClickSound.Play();
+        mainPanel.SetActive(false);
+        quitGamePanel.SetActive(true);
+    }
+
+    public void OnClickQuitGameNo()
+    {
+        //buttonClickSound.Play();
+        quitGamePanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
+    public void OnClickQuitGameYes()
+    {
+        //buttonClickSound.Play();
+        Application.Quit();
+    }
+
+    public void OnClickBackFromPlay()
+    {
+        //buttonClickSound.Play();
+        playPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
+    public void OnClickBackFromSettings()
+    {
+        //buttonClickSound.Play();
+        settingsPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
+    #endregion
+
+    #region settings
+
+    #endregion
+}
